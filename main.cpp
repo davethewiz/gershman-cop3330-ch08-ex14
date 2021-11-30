f139e7d81b0172e11b9c84e1625e95a9ec52153c
Dave <davegershman465@gmail.com>
2021-11-29 23:25:56 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 5 Solution
+ *  Copyright 2021 Dave Gershman
+ */
+ 
+#include <iostream>
+using namespace std;
+
+void modifyNum(int num) {
+    num = 2;
+}
+
+int add2_const(const int num) {
+    return num + 2;
+}
+
+int add2(int num) {
+    return num + 2;
+}
+
+int main()
+{
+    return 0;
+}
